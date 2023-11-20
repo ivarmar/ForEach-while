7dd9a35b7e24fc2d110a8aa48bb03080ed343f6f
ivarmar <141323294+ivarmar@users.noreply.github.com>
2023-11-20 10:39:53 +0100

    Add project files.

---
+++ ForEach/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ForEach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string: ");
+            string userInput = Console.ReadLine();
+            
+            Console.Write("Reversed string: ");
+            for (int i = userInput.Length -1; i >= 0; i--) 
+            {
+                Console.Write(userInput[i]);
+            }
+
+
+
+            //
+            
+            
+            int rundenummer = 0;
+
+            while (rundenummer < 11)
+            {
+                Console.WriteLine($"Runde nummer: {rundenummer}/10 ");
+                rundenummer++;
+            }
+
+            
+
+
+        }
+    }
+}
